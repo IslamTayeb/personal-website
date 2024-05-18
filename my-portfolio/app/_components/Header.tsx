e635066efb4ae0ba48bc4425f4c74e0b86a59491
Islam Tayeb <imt11@duke.edu>
2024-05-18 19:21:29 +0300

    Set up

---
+++ my-portfolio/app/_components/Header.tsx
@@ -0,0 +1,12 @@
+import { Section } from "./Section"
+
+export const Header = () => {
+    return (
+        <header>
+            <Section>
+                <h1 className="text-4xl font-bold text-primary-foreground">islamtayeb.io</h1>
+            </Section>
+        </header>
+    )
+
+}
---
+++ my-portfolio/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Anek_Telugu } from "next/font/google";
+import "./globals.css";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import { cn } from "@/lib/utils";
+
+const anekTelugu = Anek_Telugu({
+  subsets: ["latin"],
+  variable: "--font-caption",
+});
+
+export const metadata: Metadata = {
+  title: "Islam Tayeb • Website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={cn(GeistSans.variable, GeistMono.variable, anekTelugu, "font-mono h-full bg-background text-foreground")}>{children}</body>
+    </html>
+  );
+}
