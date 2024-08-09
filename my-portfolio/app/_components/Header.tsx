f777c41ebae76924478e5c51ec5d757e28e55f5e
Islam M. Tayeb <islam.tayeb@duke.edu>
2024-08-09 06:35:33 -0400

    Links lead to new tab

--- my-portfolio/app/_components/Header.tsx
+++ my-portfolio/app/_components/Header.tsx
@@ -9,18 +9,19 @@ import { ArrowBigDown, Layers2, Scroll, ScrollText } from "lucide-react";
 import { Code, DefaultIcon } from "./sharedComponents";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
-
-
 export const Header = () => {
   return (
     <header className="sticky top-0 py-3 z-50 bg-card bg-clip-padding bg-opacity-80">
       <Section className="flex items-baseline">
-        <h1 className="text-lg font-semibold text-primary"><Link href="#hero">islamtayeb.dev</Link></h1>
+        <h1 className="text-lg font-semibold text-primary">
+          <Link href="#hero">islamtayeb.dev</Link>
+        </h1>
         <div className="flex-1" />
         <ul className="flex items-center gap-2">
-
           <Link
             href="https://linkedin.com/in/islam-tayeb"
+            target="_blank"
+            rel="noopener noreferrer"
             className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
           >
             <LinkedInIcon size={12} className="text-foreground" />
@@ -28,23 +29,40 @@ export const Header = () => {
 
           <Link
             href="https://github.com/IslamTayeb"
+            target="_blank"
+            rel="noopener noreferrer"
             className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
           >
-            <DefaultIcon fontSize={15.25} className="text-foreground -mt-[0.5px]" icon={"jam:github"} />
+            <DefaultIcon
+              fontSize={15.25}
+              className="text-foreground -mt-[0.5px]"
+              icon={"jam:github"}
+            />
           </Link>
 
           <Link
             href="https://www.researchgate.net/profile/Islam-Tayeb"
+            target="_blank"
+            rel="noopener noreferrer"
             className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
           >
             <Icon icon={"academicons:researchgate-square"} className="" />
           </Link>
           <Link
             href="Islam_Tayeb_Resume.pdf"
-            className={cn(buttonVariants({ variant: "outline" }), "px-[4.5px] h-6")}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={cn(
+              buttonVariants({ variant: "outline" }),
+              "px-[4.5px] h-6"
+            )}
           >
-            <DefaultIcon fontSize={13} className="text-foreground -mt-0.5" icon={"solar:file-bold"} />
-            
+            <DefaultIcon
+              fontSize={13}
+              className="text-foreground -mt-0.5"
+              icon={"solar:file-bold"}
+            />
+
             <p className="text-foreground m-0 leading-none text-xs inline max-sm:hidden ml-1">
               Resume
             </p>
