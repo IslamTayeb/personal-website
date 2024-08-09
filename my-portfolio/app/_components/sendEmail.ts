a43f11189f7c8b702aa6420801cc259292601d19
Islam M. Tayeb <islam.tayeb@duke.edu>
2024-08-09 05:09:03 -0400

    Update sendEmail.ts

--- my-portfolio/app/_components/sendEmail.ts
+++ my-portfolio/app/_components/sendEmail.ts
@@ -41,7 +41,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "onboarding@islamtayeb.dev",
+      from: "Portfolio <send@islamtayeb.dev>",
       subject: subject as string,
       reply_to: email as string,
       to: "islam.tayeb@duke.edu",
