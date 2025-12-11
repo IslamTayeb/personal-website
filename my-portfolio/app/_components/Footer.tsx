6bf9e5d9d7c41c2b83cf18bfcd0e4aa847b43c1c
Islam M. Tayeb <islam.tayeb@duke.edu>
2025-12-11 02:48:56 -0500

    update latest updated

--- my-portfolio/app/_components/Footer.tsx
+++ my-portfolio/app/_components/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
     <footer className="bg-card border-border border-t border-dashed">
       <div className="py-3.5 text-muted-foreground text-sm flex gap-x-1 max-w-3xl m-auto px-4">
         <span className="">
-          Last Updated 12/03/2025
+          Last Updated 12/11/2025
         </span>
         <div className="ml-auto text-right max-[375px]:hidden">
           Made with <HeartFilledIcon className="inline -mt-1" width={11.5} /> in{' '}
